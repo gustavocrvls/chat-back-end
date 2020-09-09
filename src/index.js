b7f8e2701bfbc118f9cc5d8e1e0d5b0e964b9374
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2020-09-09 11:44:35 -0300

    Configuração da Aplicação usando .env

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 3001;
+dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,7 +15,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 
-require('./config/database')('mongodb://localhost', 'sirius');
+require('./config/database')(process.env.DB_URI, process.env.DB_NAME);
 
 io.on('connection', (socket) => {
     socket.on('chat new message', (msg) => {
@@ -19,6 +23,6 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3333, () => {
-    console.log('Server on Port 3333');
+http.listen(PORT, () => {
+    console.log(`Server on Port ${PORT}`);
 });
